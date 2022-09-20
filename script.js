374b136ba74e207cd1991296252944f034c126ee
Testpilot <113631739+mugennotaikutsu@users.noreply.github.com>
2022-09-20 14:36:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+let options = [
+    "Endless Boredom",
+    "NGL",
+    "NGL Replies",
+    ":)"
+];
+let links = [
+    "",
+    "https://ngl.link/deeptanshu0_07",
+    "nglreplies.html",
+    "=).html"
+]
+let current = 0;
+let isHovering = false;
+
+function translateWelcome() {
+    document.querySelector(':root').style.setProperty("--cursor", "none");
+    document.getElementsByClassName("welcome")[0].style.fontFamily = 'Dancing Script'
+    document.getElementsByClassName("welcome")[0].innerHTML = options[current];
+    seenbefore = localStorage.getItem("seenbefore")
+    if (!seenbefore) {
+        alert("arrow keys then enter");
+        localStorage.setItem("seenbefore", true);
+    }
+}
+
+document.addEventListener("keydown", (event) => {
+    let name = event.key;
+    if (name.toLowerCase() == "d" || name == "ArrowRight") {
+        current++;
+    } else if (name.toLowerCase() == "a" || name == "ArrowLeft") {
+        current--;
+    } else if (name == "Enter") {
+        if (current != 0) document.location = links[current];
+        else alert("thiswaspainfultomake 😢")
+    }
+    if (current >= options.length) current = 0;
+    else if (current < 0) current = options.length - 1;
+    document.getElementsByClassName("welcome")[0].innerHTML = options[current];
+}, false);
